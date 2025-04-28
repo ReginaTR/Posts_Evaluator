83aa8fdcb730733e08e09a6a85966dc7d50ef5a8
ReginaTR <regininhar@hotmail.com>
2025-04-28 17:24:36 -0300

    IPs by authors and tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts, only: [ :create ] do
     collection do
       get :top_posts
+      get :ips_by_authors
     end
   end
   resources :ratings, only: [ :create ]
